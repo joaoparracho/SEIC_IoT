fd50e5838315d6818f2064e6544e208436e4cf38
joaoparracho <35959030+joaoparracho@users.noreply.github.com>
2019-06-14 21:30:03 +0100

    #1

---
+++ MQTT/sloeber.ino.cpp
@@ -0,0 +1,27 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2019-06-14 21:21:40
+
+#include "Arduino.h"
+#include "Arduino.h"
+#include "string.h"
+#include "stdint.h"
+#include <ELClient.h>
+#include <ELClientCmd.h>
+#include <ELClientMqtt.h>
+
+void wifiCb(void* response) ;
+void publishData();
+void mqttConnected(void* response) ;
+void mqttDisconnected(void* response) ;
+void mqttData(void* response) ;
+void mqttPublished(void* response) ;
+void setup() ;
+void loop() ;
+
+#include "MQTT.ino"
+
+
+#endif
